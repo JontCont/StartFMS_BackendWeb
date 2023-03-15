8fdd2624f4d3b57137549fcdbf516005fa055a0b
jontcont <52921053+JontCont@users.noreply.github.com>
2023-03-15 15:08:12 +0800

    移除不必要的套件

--- startfms_backend_vue/src/components/@Shared/LayoutView.vue
+++ startfms_backend_vue/src/components/@Shared/LayoutView.vue
@@ -26,6 +26,7 @@
 </template>
   
 <script>
+  // 公用 Layout compoent
   import HeaderPage from './Header/HeaderPage.vue'
   import HeaderContentPage from './Header/HeaderContentPage.vue'
   import MenuPage from './Menu/MenuPage.vue'
@@ -49,42 +50,9 @@
 /*Google Font: Source Sans Pro*/
 @import 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback';
 @import 'https://fonts.googleapis.com/css2?family=Noto+Sans&family=Noto+Sans+TC&display=swap" rel="stylesheet';
-/* Ionicons */
-@import 'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css';
 
-@import '../../assets/lib/jqvmap/jqvmap.min.css';
+/* adminlte */
 @import '../../assets/lib/adminlte/css/adminlte.min.css';
-@import '../../assets/lib/overlayScrollbars/css/OverlayScrollbars.min.css';
-@import '../../assets/lib/daterangepicker/daterangepicker.css';
-@import '../../assets/lib/summernote/summernote-bs4.min.css';
-@import '../../assets/lib/sweetalert2/sweetalert2.min.css';
-
-/* Select2 */
-@import '../../assets/lib/select2/css/select2.min.css';
-@import '../../assets/lib/select2-bootstrap4-theme/select2-bootstrap4.min.css';
-
-
-
-/*----- Script ------
-@import '../../assets/lib/bootstrap/dist/js/bootstrap.bundle.min.js';
-@import '../../assets/lib/bootstrap-select-1.13.14/dist/js/bootstrap-select.min.js';
-@import '../../assets/lib/bootstrap-table/dist/bootstrap-table.min.js';
-@import '../../assets/lib/bootstrap-table/dist/bootstrap-table-locale-all.min.js';
-@import '../../assets/lib/bootstrap-table/dist/extensions/mobile/bootstrap-table-mobile.min.js';
-@import '../../assets/lib/bootstrap-table/dist/extensions/export/bootstrap-table-export.min.js';
-@import '../../assets/lib/bootstrap-table/dist/extensions/filter-control/bootstrap-table-filter-control.min.js';
-@import '../../assets/lib/bootstrap-table/dist/extensions/resizable/bootstrap-table-resizable.min.js';
-@import '../../assets/js/jspdf.min.js';
-@import '../../assets/js/tableExport.min.js';
-@import '../../assets/js/jspdf.plugin.autotable.js';
-@import '../../assets/js/jquery.resizableColumns.min.js';
-
-@import '../../assets/js/Tabullet.js';
-@import '../../assets/lib/jQuery-BlockUI/jquery.blockUI.js';
-@import '../../assets/lib/sweetalert/sweetalert.min.js';
-*/
-
-
 
 
 
