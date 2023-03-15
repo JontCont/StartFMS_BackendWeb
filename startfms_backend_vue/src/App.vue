4075b2a1cfd9a79ebaac3e2f809b4d15111d640f
jontcont <52921053+JontCont@users.noreply.github.com>
2023-03-15 11:17:58 +0800

    調整風格

--- startfms_backend_vue/src/App.vue
+++ startfms_backend_vue/src/App.vue
@@ -1,26 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <HeaderPage></HeaderPage>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HeaderPage from './components/@Shared/Header/HeaderPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HeaderPage
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- startfms_backend_vue/src/main.js
+++ startfms_backend_vue/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import Layout from './components/@Shared/LayoutView.vue'
 
-createApp(App).mount('#app')
+var app = createApp(Layout);
+document.body.classList.add('hold-transition');
+document.body.classList.add('sidebar-mini');
+document.body.classList.add('layout-fixed');
+
+app.mount('#app')
