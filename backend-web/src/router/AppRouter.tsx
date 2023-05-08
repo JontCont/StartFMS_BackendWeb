edc967d7d3f7915177edebb4014826172d4d4103
jontcont <52921053+JontCont@users.noreply.github.com>
2023-05-08 16:26:48 +0800

    加入規則

--- backend-web/src/router/AppRouter.tsx
+++ backend-web/src/router/AppRouter.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Header from '../component/@Shared/Header';
+// append page (only views)
 import Home from '../component/template-page/Home';
 import About from '../component/template-page/About';
-import Header from '../component/@Shared/Header';
 
 const AppRouter = () => {
     return (
