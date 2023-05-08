592333d56219d99d9253418029f4384cb2da7fc5
jontcont <52921053+JontCont@users.noreply.github.com>
2023-05-08 16:52:55 +0800

    增加設定位置 (page 主要設定位置)

--- backend-web/src/router/AppRouter.tsx
+++ backend-web/src/router/AppRouter.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+//append css or js
+import 'admin-lte/dist/css/adminlte.min.css'
+import 'admin-lte/dist/js/adminlte'
+import 'admin-lte/plugins/fontawesome-free/css/all.css'
+//append layout
 import Header from '../component/@Shared/Header';
+import MenuSidebar from '../component/@Shared/MenuSidebar';
 // append page (only views)
 import Home from '../component/template-page/Home';
 import About from '../component/template-page/About';
@@ -10,6 +16,8 @@ const AppRouter = () => {
         <BrowserRouter>
             <div className="wrapper">
                 <Header />
+                <MenuSidebar/>
+                
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
