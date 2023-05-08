f3585b06ad4ecbe8715c984836c729d9f65a31f4
jontcont <52921053+JontCont@users.noreply.github.com>
2023-05-08 16:13:02 +0800

    移出多餘的內容

--- backend-web/src/index.tsx
+++ backend-web/src/index.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import HelloWorld from './component/template/HelloWorld';
 import AppRouter from './router/AppRouter';
-
+// import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-// root.render(
-//   <React.StrictMode>
-//     <HelloWorld />
-//   </React.StrictMode>
-// );
 
 root.render(
   <AppRouter />
