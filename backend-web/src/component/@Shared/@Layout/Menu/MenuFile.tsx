012109602fdd272f3ad5983edff82ea18b6fcfab
jontcont <52921053+JontCont@users.noreply.github.com>
2023-05-14 22:59:38 +0800

    修改連結

--- backend-web/src/component/@Shared/@Layout/Menu/MenuFile.tsx
+++ backend-web/src/component/@Shared/@Layout/Menu/MenuFile.tsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 
 
 const MenuFile = ({ name, url = "/", icon = "far fa-circle nav-icon" }: any) => {
     return (
-        <a href={url} className="nav-link">
+        <Link to={url} className="nav-link">
             <i className={`nav-icon ${icon} `}></i>
             <p>{name}</p>
-        </a>
+        </Link>
     );
 }
 
