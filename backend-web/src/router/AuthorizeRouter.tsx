9e58a2d333cb0754b9889b3d4b52c29200f8400f
jontcont <52921053+JontCont@users.noreply.github.com>
2023-05-27 15:40:16 +0800

    Create AuthorizeRouter.tsx
    定義 : Login 、註冊 、第三方等相關

---
+++ backend-web/src/router/AuthorizeRouter.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+
+
+const Authorize = () => {
+    return (
+        <BrowserRouter>
+        </BrowserRouter>
+    );
+}
+
+export default Authorize;
