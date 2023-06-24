9477a10f1fe8785918df157043c6b7eac6c8f29b
jontcont <52921053+JontCont@users.noreply.github.com>
2023-06-24 10:24:07 +0800

    primereact

--- src/component/@Views/TopList/About.tsx
+++ src/component/@Views/TopList/About.tsx
@@ -1,44 +1,57 @@
-// About.js
-import React from 'react';
-const About = () => {
-  return (
-    <div>
-      <section className="content-header">
-        <div className="container-fluid">
-          <div className="row mb-2">
-            <div className="col-sm-6">
-              <h1>About</h1>
-            </div>
-            <div className="col-sm-6">
-              <ol className="breadcrumb float-sm-right">
-                <li className="breadcrumb-item active">About</li>
-              </ol>
-            </div>
-          </div>
-        </div>
-      </section>
+import React, { useRef } from 'react';
+import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
+import { Toast } from 'primereact/toast';
+import { Button } from 'primereact/button';
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";     
 
-      <section className="content">
-        <div className="container-fluid">
-          <div className="card card-primary card-outline">
-            <div className="card-header">
-              <h3 className="card-title">About</h3>
-            </div>
-            <div className="card-body">
-              <p>Learn more about me and my skills!</p>
-              <p>You can use any font library you like with AdminLTE 3.</p>
-              <strong>Recommendations</strong>
-              <div>
-                <a href="https://fontawesome.com/">Font Awesome</a>
-                <a href="https://useiconic.com/open/">Iconic Icons</a>
-                <a href="https://ionicons.com/">Ion Icons</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
-  );
+const About = () => {
+  const toast = useRef(null);
+
+  const accept = () => {
+    alert(true);
+    // toast.current.show({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted', life: 3000 });
+  }
+
+  const reject = () => {
+    alert(false);
+    // toast.current.show({ severity: 'warn', summary: 'Rejected', detail: 'You have rejected', life: 3000 });
+  }
+
+  const confirm1 = () => {
+    confirmDialog({
+      message: 'Are you sure you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept,
+      reject
+    });
+  };
+
+  const confirm2 = () => {
+    confirmDialog({
+      message: 'Do you want to delete this record?',
+      header: 'Delete Confirmation',
+      icon: 'pi pi-info-circle',
+      acceptClassName: 'p-button-danger',
+      accept,
+      reject
+    });
+  };
+
+  return (
+    <>
+      <Toast ref={toast} />
+      <ConfirmDialog />
+      <div className="card flex flex-wrap gap-2 justify-content-center">
+        <Button onClick={confirm1} icon="pi pi-check" label="Confirm" className="mr-2"></Button>
+        <Button onClick={confirm2} icon="pi pi-times" label="Delete"></Button>
+      </div>
+    </>
+  )
 };
 
 export default About;
