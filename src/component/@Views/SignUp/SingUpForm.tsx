8bdfac09f061742f722a88f6c797cbcf467dcc75
ConteMA <52921053+JontCont@users.noreply.github.com>
2024-08-10 16:43:02 +0800

    Update SingUpForm.tsx

--- src/component/@Views/SignUp/SingUpForm.tsx
+++ src/component/@Views/SignUp/SingUpForm.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import "./SingUp.css";
 import "react-toastify/dist/ReactToastify.css";
+import { SignUp } from "../../../models/System/SignUp";
+import { Link } from "react-router-dom";
 
 const SingUpForm = () => {
+  let [formData, setFormData] = useState(new SignUp());
+
   return (
     <div className="h-100vh d-flex justify-content-center">
       <div className="align-self-center">
@@ -9,7 +14,29 @@ const SingUpForm = () => {
           <div className="card-header text-center">
             <h3>註冊</h3>
           </div>
+          <div className="card-body">
+            <div className="form-row">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                className="form-control"
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+              />
+            </div>
 
+            <div className="mt-6 input-buttons">
+              <input type="button" className="btn btn-primary" value="註冊" />
+              <Link className="btn btn-danger ml-2" to="/login">
+                返回
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
